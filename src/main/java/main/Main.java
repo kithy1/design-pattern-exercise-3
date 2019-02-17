8800154ed27ee3b5df2d1e44f2981f9fb547f469
Katarzyna <kithy@interia.eu>
2019-02-17 13:53:00 +0100

    smacznego 1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,13 +7,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        CapriciosaPizza capriciosaPizza = new CapriciosaPizza(31.99,Arrays.asList("cheese","champignons","ham"));
-        HawaiPizza hawaiPizza = new HawaiPizza(29.99,Arrays.asList("cheese","pineaple","ham"));
-        MargheritaPizza margheritaPizza = new MargheritaPizza(24, Arrays.asList("cheese"));
+//        CapriciosaPizza capriciosaPizza = new CapriciosaPizza(31.99,Arrays.asList("cheese","champignons","ham"));
+//        HawaiPizza hawaiPizza = new HawaiPizza(29.99,Arrays.asList("cheese","pineaple","ham"));
+//        MargheritaPizza margheritaPizza = new MargheritaPizza(24, Arrays.asList("cheese"));
+        PizzaFactory pizzaFactory = new PizzaFactory();
+        Pizza pizza1 = pizzaFactory.create(PizzaType.MARGHERITA,15.99,Arrays.asList("cheese"));
+        Pizza pizza2 = pizzaFactory.create(PizzaType.FRUTTIDIMARE,25.00,Arrays.asList("cheese","shrimps"));
 
-        capriciosaPizza.showPizzaInfo();
-        hawaiPizza.showPizzaInfo();
-        margheritaPizza.showPizzaInfo();
+        pizza1.showPizzaInfo();
+        pizza2.showPizzaInfo();
+
     }
 
 }
--- src/main/java/pizza/PizzaFactory.java
+++ src/main/java/pizza/PizzaFactory.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class PizzaFactory {
     public Pizza create(PizzaType pizzaType, double coast, List<String> components) {
         if (pizzaType.equals(PizzaType.CAPRICIOSA)) {
-            return new CapriciosaPizza(15.90, null);
+            return new CapriciosaPizza(coast, components);
         } else if (pizzaType.equals(PizzaType.FRUTTIDIMARE)){
-           return new FruttiDiMare(20.00,null);
+           return new FruttiDiMare(coast,components);
         }else if (pizzaType.equals(PizzaType.HAWAI)){
-            return new HawaiPizza(25.00,null);
+            return new HawaiPizza(coast,components);
         }else if (pizzaType.equals(PizzaType.MARGHERITA)){
-            return new MargheritaPizza(12.50,null);
+            return new MargheritaPizza(coast,components);
         }
         return null;
     }
